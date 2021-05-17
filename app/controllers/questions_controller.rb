06f1c37add442bc479fb2878f2c5b5ebddef0d6a
Thomas Sanders <thomas.a.sanders@gmail.com>
2021-05-17 12:55:26 +0900

    I think the logic is done but I can't get it to post

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,16 @@
+class QuestionsController < ApplicationController
+
+  def ask
+  end
+
+  def answer
+    if params[:user_message] == "I am going to work"
+      @answer = "Great!"
+    elsif params[:user_message].include?("?")
+      @answer = "Silly question, get dressed and go to work!"
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
+  end
+
+end
